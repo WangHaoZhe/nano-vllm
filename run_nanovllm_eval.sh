afb6a63b3deda4a3b9ba0bee3c594038ff1bfc99
Albert Wang <albert_wang@sjtu.edu.cn>
2026-04-05 01:29:07 +0800

    test: add lm-eval test
    Signed-off-by: Albert Wang <albert_wang@sjtu.edu.cn>

---
+++ run_nanovllm_eval.sh
@@ -0,0 +1,12 @@
+MODEL_NAME="mit-han-lab/Llama-3-8B-Instruct-QServe"
+# MODEL_NAME="Meta-Llama-3-8B-Instruct"
+MODEL_PATH="http://localhost:8123/v1/chat/completions"
+TOKENIZER_PATH=${MODEL_PATH}
+
+lm_eval     --model local-chat-completions \
+    --model_args model=${MODEL_NAME},base_url=${MODEL_PATH},num_concurrent=1,max_retries=3,max_length=1000     \
+    --tasks mmlu_generative     \
+    --output_path ./eval_out    \
+    --log_samples   \
+    --cache_requests true    \
+    --apply_chat_template
